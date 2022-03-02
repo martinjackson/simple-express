ac3eb3c2477023ab02b82dd68611c3bd319f258e
Martin A. Jackson <martin.a.jackson>
2022-03-02 11:59:18 -0600

    updated on - Wed Mar  2 11:59:18 AM CST 2022

--- appServer.js
+++ appServer.js
@@ -24,7 +24,7 @@ const addMonitorRoutes = require('./apiMonitorRoutes.js');
 
 
 // -----------------------------------------------------------------------------------------------
-function serve(makeRouter, dotEnvPath) {
+const serve = async (makeRouter, dotEnvPath) => {
 
     console.log('dotenv: ', dotEnvPath);
     const result = dotenv.config({ path: dotEnvPath })
