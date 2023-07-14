3eb547fb2cc8f47697f3f723c145c7f757a77c3e
Martin A. Jackson <Martin.Jackson@fda.hhs.gov>
2023-07-14 09:58:10 -0500

    added debug listing of default ciphers and setting siphers, prep for SSL Server Test

--- sslOptions.js
+++ sslOptions.js
@@ -1,16 +1,46 @@
 
-const fs = require('fs');
-const { exit } = require('process');
-// const path = require('path');
-// const os = require('os');
+const fs = require('fs')
+const tls = require('tls')
+const { exit } = require('process')
+// const path = require('path')
+// const os = require('os')
 
 const genSSLOptions = (fqdn) => {
+
+   // taken from io.js' TLS docs
+   // https://iojs.org/api/tls.html
+
+   var defCiphers = tls.getCiphers()
+   console.log('default ciphers:', defCiphers)    // ['AES128-SHA', 'AES256-SHA', ...]
+
+  const ciphers = [
+    "ECDHE-RSA-AES256-SHA384",
+    "DHE-RSA-AES256-SHA384",
+    "ECDHE-RSA-AES256-SHA256",
+    "DHE-RSA-AES256-SHA256",
+    "ECDHE-RSA-AES128-SHA256",
+    "DHE-RSA-AES128-SHA256",
+    "HIGH",
+    "!aNULL",
+    "!eNULL",
+    "!EXPORT",
+    "!DES",
+    "!RC4",
+    "!MD5",
+    "!PSK",
+    "!SRP",
+    "!CAMELLIA"
+].join(':')
+
+    console.log('setting ciphers:', ciphers) // ['AES128-SHA', 'AES256-SHA', ...]
 
     let sslOptions = {
         secureProtocol:'TLSv1_2_method',
         requestCert: false,
-        rejectUnauthorized: false
-      };
+        rejectUnauthorized: false,
+        ciphers: ciphers,
+        honorCipherOrder: true
+      }
 
     const sslDirs = ['/opt/Certs/', 'ssl/']
     const sslDir = sslDirs.find(dir => fs.existsSync(dir+fqdn+'.key') )
