cf04c9684a52dc409cc580bcaf436dce1392c3bc
Martin A. Jackson <Martin.Jackson@fda.hhs.gov>
2023-07-28 09:18:12 -0500

    working on insuring the last 4k of log file is there -- why the crash.

--- server-log.js
+++ server-log.js
@@ -1,17 +1,37 @@
 
-const fs = require('fs');
-require('log-timestamp');           // add time stamp to every line logged, console.log(), etc.
+const fs = require('fs')
+require('log-timestamp')     // add time stamp to every line logged, console.log(), etc.
 
-const { date2str } = require("./date2str");
+const { date2str } = require("./date2str")
 
+let saveStdOut = null
+let saveStdErr = null
+let logger = null
+
+// ----------------------------------------------------------------------------------------------------
+function logShutdown() {
+
+   if (saveStdOut != null)
+      process.stdout.write = saveStdOut
+
+   if (saveStdErr != null)
+      process.stderr.write = saveStdErr
+
+   if (logger != null) {
+      logger.
+   }
+
+}
+
+// ----------------------------------------------------------------------------------------------------
 function log(logFileName) {
 
   if (logFileName.endsWith('.log'))
-    logFileName = logFileName.slice(0, -4);
+    logFileName = logFileName.slice(0, -4)
 
   logFileName = logFileName + '-' + date2str(new Date(),"yyyy-MM-dd") + '.log'
 
-  var logger = fs.createWriteStream(logFileName, {flags: 'a'});  // append to existing log file
+  logger = fs.createWriteStream(logFileName, {flags: 'a'});  // append to existing log file
 
    // also echos to the console
 
@@ -20,16 +40,19 @@ function log(logFileName) {
   // process.stderr.pipe(logger)
 
   // Method 2
-  process.stdout.write = process.stderr.write = logger.write.bind(logger);
+  saveStdOut = process.stdout.write
+  saveStdErr = process.stderr.write
+
+  process.stdout.write = process.stderr.write = logger.write.bind(logger)
 
   process.on('uncaughtException', function(err) {
-    console.error((err && err.stack) ? err.stack : err);
-  });
+    console.error((err && err.stack) ? err.stack : err)
+  })
 
   console.log('========================================================');   // make restarts visible in the log file
-  console.log('Log File:', logFileName);
-  console.log('   ');
+  console.log('Log File:', logFileName)
+  console.log('   ')
 
 }
 
-module.exports = log;
+module.exports = { logShutdown, log }
---
+++ server-shutdown.js
@@ -0,0 +1,32 @@
+
+// https://stackoverflow.com/a/14032965/2634223
+// Thank you    Luis Gonzalez    https://stackoverflow.com/users/6673573/luis-gonzalez
+// Thank you    Emil Condrea     https://stackoverflow.com/users/832363/emil-condrea
+
+function shutdownSetup() {
+
+process.stdin.resume()   //so the program will not close instantly
+
+function exitHandler(options, exitCode) {
+    if (options.cleanup) console.log('clean')
+    if (exitCode || exitCode === 0) console.log('exitCode:', exitCode)
+
+    logShutdown()
+
+    if (options.exit) process.exit()
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}))
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}))
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}))
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}))
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}))
+
+}
