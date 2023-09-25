2d977b4d1d58217402ee3c78de35134fb4bb804a
Martin A. Jackson <Martin.Jackson@fda.hhs.gov>
2023-09-24 19:08:02 -0500

    cleaned up getUserFromRequest() also added compression, drops test times from 4.2s -> 3.2s, your milage may vary.

--- getUserFromRequest.js
+++ getUserFromRequest.js
@@ -1,27 +1,19 @@
 
+// https://www.pabbly.com/tutorials/express-js-request-response/
+//
+// Request Object Properties
+// req.app	    Used to hold a reference to the instance of the express application.
+// req.body	    Contains key-value pairs of data submitted in the request body. By default, it is undefined and is populated when you use body-parsing middleware such as body-parser.
+// req.cookies	This property contains cookies sent by the request, used for the cookie-parser middleware.
+// req.ip	      req.ip is remote IP address of the request.
+// req.path	    req.path contains the path part of the request URL.
+// req.route    req.route is the currently-matched route.
+
 // ---------------------------------------------------------
 function getUserFromRequest(req) {
 
-  // https://www.pabbly.com/tutorials/express-js-request-response/
-  //
-  // Request Object Properties
-  // req.app	    Used to hold a reference to the instance of the express application.
-  // req.body	    Contains key-value pairs of data submitted in the request body. By default, it is undefined and is populated when you use body-parsing middleware such as body-parser.
-  // req.cookies	This property contains cookies sent by the request, used for the cookie-parser middleware.
-  // req.ip	      req.ip is remote IP address of the request.
-  // req.path	    req.path contains the path part of the request URL.
-  // req.route    req.route is the currently-matched route.
-
-  const ip = req.ip
-  let user = '-'
-
-  if (req.cookies) {
-    user = req?.cookies["NCTR_REMOTE_USER"] || '-'
-  }
-
-  if (req?.session?.user?.name) {
-    user = req?.session?.user?.name
-  }
+  const ip = req?.ip || '?.?.?.?'
+  const user = req?.session?.user?.name || '-'
 
   return {user, ip}
 
